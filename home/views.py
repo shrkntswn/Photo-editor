8242867c860175046e05558ce7a45675d813b207
Shrikant <shrkntswn@gmail.com>
2020-12-01 05:17:40 +0530

    views file created

--- home/views.py
+++ home/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from .forms import PhotoForm
+from .models import Photo
+
+def photo_list(request):
+	photos = Photo.objects.all()
+	if request.method == 'POST':
+		form = PhotoForm(request.POST, request.FILES)
+		if form.is_valid():
+			form.save()
+			return redirect('photo_list')
+
+		else:
+			form = PhotoForm()
+		return render(request, 'home/photo_list.html')
