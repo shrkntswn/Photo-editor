78020db66686924cbe4a193d5974fb0f19276dee
Shrikant <shrkntswn@gmail.com>
2020-12-02 07:41:32 +0530

    description characters reduced

--- home/models.py
+++ home/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Photo(models.Model):
 	file = models.ImageField()
-	description = models.CharField(max_length=255, required=False)
+	description = models.CharField(max_length=100)
 	uploaded_at = models.DateTimeField(auto_now_add=True)
 
 	class meta:
