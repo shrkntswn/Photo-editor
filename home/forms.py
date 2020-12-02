be76be22408c3366021b57e4bcdccb8b71eb4a6a
Shrikant <shrkntswn@gmail.com>
2020-12-02 07:40:10 +0530

    description field added

--- home/forms.py
+++ home/forms.py
@@ -1,27 +1,28 @@
 from PIL import Image
 from django import forms
 from .models import Photo
+from django.core.files import File
+
 
 class PhotoForm(forms.ModelForm):
-	x = forms.FloatField(widget= forms.hiddenInput())  
-	y = forms.FloatField(widget= forms.hiddenInput())  
-	width = forms.FloatField(widget= forms.hiddenInput())  
-	height = forms.FloatField(widget= forms.hiddenInput()) 
-
+	x = forms.FloatField(widget= forms.HiddenInput())  
+	y = forms.FloatField(widget= forms.HiddenInput())  
+	width = forms.FloatField(widget= forms.HiddenInput())  
+	height = forms.FloatField(widget= forms.HiddenInput()) 
+	description = forms.CharField(label='Description', widget=forms.Textarea(attrs={'class': 'form-control', 'rows':3,}))
 	class Meta:
 		model = Photo
-		fields = ('file', 'x', 'y', 'width', 'height')
+		fields = ('description', 'file', 'x', 'y', 'width', 'height',)
 
-		def save(self):
-			photo = super(PhotoForm, self).save()
-			x = self.cleaned_data.get('x')
-			y = self.cleaned_data.get('y')
-			w = self.cleaned_data.get('width')
-			h = self.cleaned_data.get('height')
+	def save(self):
+		photo = super(PhotoForm, self).save()
+		x = self.cleaned_data.get('x')
+		y = self.cleaned_data.get('y')
+		w = self.cleaned_data.get('width')
+		h = self.cleaned_data.get('height')
 
-			image = Image.open(photo.file)
-			cropped_image = image.crop((x, y, w+x, h+y))
-	        resized_image = cropped_image.resize((300, 300), Image.ANTIALIAS)
-	        resized_image.save(photo.file.path)
-
-	        return photo
+		image = Image.open(photo.file)
+		cropped_image = image.crop((x, y, w+x, h+y))
+		resized_image = cropped_image.resize((200, 200), Image.ANTIALIAS)
+		resized_image.save(photo.file.path)
+		return photo
