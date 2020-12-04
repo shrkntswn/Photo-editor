b8c5282249986ca59efb756d8b40cbe9dcfcf189
Shrikant <shrkntswn@gmail.com>
2020-12-04 16:37:50 +0530

    slight modifications

--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from .views import photo_list,photo_blend
 
-app_name='home'
+
 urlpatterns = [
     path('home/', photo_list, name="photo_list"),
-    path('blend', photo_blend, name="photo_blend"),
+    path('blend/', photo_blend, name="photo_blend"),
 ]
